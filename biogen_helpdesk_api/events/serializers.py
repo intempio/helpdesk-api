10f87242004c40f77db42b9ed93462dbe0e6825a
Rustan Corpuz <rustanacexd@gmail.com>
2018-06-02 04:20:58 +0800

    add many to many create function

--- biogen_helpdesk_api/events/serializers.py
+++ biogen_helpdesk_api/events/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Event, Attendee, EventAttendee
+from.models import Event, Attendee, EventAttendee
 
 class EventSerializer(serializers.ModelSerializer):
 
@@ -15,7 +15,22 @@ class AttendeeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class EventAttendeeSerializer(serializers.ModelSerializer):
+    attendee = AttendeeSerializer()
+    event = EventSerializer()
 
     class Meta:
         model = EventAttendee
         fields = '__all__'
+
+    def create(self, validated_data):
+        event_data = validated_data.pop('event')
+        attendee_data = validated_data.pop('attendee')
+
+        event = Event.objects.create(**event_data)
+        attendee = Attendee.objects.create(**attendee_data)
+
+        instance = EventAttendee.objects.create(
+            event=event, attendee=attendee, **validated_data
+        )
+
+        return instance
--- biogen_helpdesk_api/events/views.py
+++ biogen_helpdesk_api/events/views.py
@@ -1,4 +1,6 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from .models import Attendee, Event, EventAttendee
 from .serializers import (AttendeeSerializer, EventAttendeeSerializer,
@@ -19,5 +21,11 @@ class AttendeeViewSet(viewsets.ModelViewSet):
 
 class EventAttendeeViewSet(viewsets.ModelViewSet):
 
-    queryset = EventAttendee.objects.all()
+    queryset = EventAttendee.objects.all().select_related('attendee', 'event')
     serializer_class = EventAttendeeSerializer
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter)
+    filter_fields = (
+        'attendee__first_name', 'attendee__last_name', 'attendee__email',
+        'event__event_name', 'event__event_type', 'event__program_id'
+    )
+    search_fields = ('attendee__first_name', 'attendee__last_name', 'attendee__email')
