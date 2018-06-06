eae1d97e764470bac7419a9a6493ae61a2335a32
Rustan Corpuz <rustanacexd@gmail.com>
2018-06-07 00:30:10 +0800

    add eventserailzier

--- biogen_helpdesk_api/events/serializers.py
+++ biogen_helpdesk_api/events/serializers.py
@@ -23,7 +23,7 @@ class EventAttendeeReadOnlySerializer(serializers.ModelSerializer):
     class Meta:
         model = EventAttendee
         fields = ('id', 'attendee', 'pre_registered', 'call_complete',
-                  'modified', 'created')
+                  'modified', 'created', 'event')
 
 
 class AttendeeSerializer(serializers.ModelSerializer):
